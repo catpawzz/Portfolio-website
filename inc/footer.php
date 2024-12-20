e05a7e631f11047c7d61ada550b7f728a14d0ea9
catpawz <cat@catpawz.net>
2024-12-20 15:23:00 +0100

    Completed the footer

--- inc/footer.php
+++ inc/footer.php
@@ -6,7 +6,7 @@
                         <li><a href="mailto:tom@tombugada.eu" class=" text-purple-200 hover:text-purple-300">Contact</a></li>
                     </ul>
                     <span class="text-lg text-gray-100 text-center block">©<a href="https://tombugada.eu/">Tom Bugada</a> 2024 - Present, All rights reserved.</span>
-                    <p class="text-sm text-gray-400 text-center mt-4">This page does not collect any statistics or data about you, and does not store any cookies in your browser</p>
+                    <p class="text-sm text-gray-400 text-center mt-4">This page does not collect any statistics or data about you, and does not store any cookies in your browser :)</p>
             </div>
         </div>
     </footer>
